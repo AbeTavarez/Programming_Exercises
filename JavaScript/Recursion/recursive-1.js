c5b5eb509989b5976c276accc431868ace826e7d
AbeTavarez <abeincwireless@gmail.com>
2021-05-22 23:26:24 -0400

    recursive exe-1

--- JavaScript/Recursion/recursive-1.js
+++ JavaScript/Recursion/recursive-1.js
@@ -1,5 +1,13 @@
 function countDown(num){
     // Base case
+    if (num <= 0){
+        console.log('Yayy!!')
+        return ;
+    }
+    console.log(num)
+    num = num -1
+    // Recursive Call\
+    countDown(num)
+};
 
-    // Recursive Call
-}
+countDown(5)

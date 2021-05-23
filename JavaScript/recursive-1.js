72594a6a7d649afe7971e5d1ed5c6696ac9d7f36
AbeTavarez <abeincwireless@gmail.com>
2021-05-22 23:19:05 -0400

    initial commit

---
+++ JavaScript/recursive-1.js
@@ -0,0 +1,21 @@
+function takeShower(){
+    return 'showering'
+}
+
+function eatBreakfast(){
+    let meal = cookFood()
+    return `Eating ${meal}`
+}
+
+function cookFood(){
+    let items = [`cereal`, `eggs`, `bacon`]
+    return items[Math.floor(Math.random() * items.length)]
+}
+
+function wakeUp(){
+    takeShower()
+    eatBreakfast()
+    console.log(`ok ready to go!`)
+}
+
+wakeUp()

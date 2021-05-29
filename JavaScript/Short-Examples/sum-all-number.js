49ed558401c022486424fbf05188fad3f67dc9c2
AbeTavarez <abeincwireless@gmail.com>
2021-05-29 19:33:13 -0400

    sum all nums

---
+++ JavaScript/Short-Examples/sum-all-number.js
@@ -0,0 +1,18 @@
+
+// const numbersSum = (num) =>{
+//     let sum = 0
+//     for (let n = 1; n <= num; n++){
+//         sum += n
+//     }
+//     return sum
+// }
+
+
+// console.log(numbersSum(3))
+
+const addUpTo = (num) => {
+    return num * (num + 1) /2
+} 
+
+
+console.log(addUpTo(3))

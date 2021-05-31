ba315940091b8b27931a59dfc95ed801e15d470c
AbeTavarez <abeincwireless@gmail.com>
2021-05-31 16:22:20 -0400

    confirm ending of string

--- JavaScript/Short-Examples/confirm-ending.js
+++ JavaScript/Short-Examples/confirm-ending.js
@@ -16,3 +16,30 @@ function confirmEnding1(str, target){
 
 console.log(confirmEnding1('Bastian', 'n'))
 
+// using string method includes()
+function confirmEnding2(str, target){
+    return str.includes(target, str.length -1)
+
+}
+
+console.log(confirmEnding2('Bastian', 'n'))
+
+
+// using string method indexOf()
+function confirmEnding3(str, target){
+    console.log(str.indexOf(target, str.length -1) === -1 ? false : true)
+
+}
+
+console.log(confirmEnding3('Bastian', 'n'))
+
+console.log('=========================WINNER=========================================')
+
+function confirmEnding4(str, target) {
+    let t_size = target.length
+    let end_str = str.slice(-t_size)
+    return target === end_str ? true : false
+    
+  }
+  
+  confirmEnding4("Bastian", "ian");

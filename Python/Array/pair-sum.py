4a57414ec1cb08f40ec34e8f71f407e2f92bf471
AbeTavarez <abeincwireless@gmail.com>
2021-06-05 23:32:57 -0400

    pair sum

--- Python/Array/pair-sum.py
+++ Python/Array/pair-sum.py
@@ -3,7 +3,7 @@
 # Given an integer array, output all the unique pairs that sum up to a specific value k
 
 
-def pai_sum(arr, k):
+def pair_sum(arr, k):
 
     # Checking lenght of array
     if len(arr) < 2:
@@ -12,3 +12,18 @@ def pai_sum(arr, k):
     # Sets for tracking
     seen = set()
     output = set()
+
+    for num in arr:
+        target = k - num
+
+        if target not in seen:
+            seen.add(num)
+        else:
+            output.add( ((min(num, target)), max(num, target)))
+    
+    #return len(output)
+    print('\n'.join(map(str, list(output))))
+
+
+
+print(pair_sum([1,3,2,2], 4))

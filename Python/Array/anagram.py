e022462965c888c1be7a8537a9366c58eba840bf
AbeTavarez <abeincwireless@gmail.com>
2021-06-05 22:45:54 -0400

    anagram1

--- Python/Array/anagram.py
+++ Python/Array/anagram.py
@@ -5,5 +5,24 @@
 
 
 def anagram1(s1,s2):
+    print(s1)
+    print(s2)
     # remove whitespace
-    s1 = s1.replace(' ', '')
+    s1 = s1.replace(' ', '').lower()
+    s2 = s2.replace(' ', '').lower()
+    # Sort strings
+    print(sorted(s1))
+    print(sorted(s2))
+    return sorted(s1) == sorted(s2)
+
+
+#print(anagram1('clint Eastwood', 'old West Action'))
+
+def anagram2(s1, s2):
+    # remove whitespace
+    s1 = s1.replace(' ', '').lower()
+    s2 = s2.replace(' ', '').lower()
+
+    # Edge case check
+    if len(s1) != len(s2):
+        return False

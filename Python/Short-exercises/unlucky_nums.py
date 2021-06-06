9d231a39ac0cacb45bbcfecafb3d39ccc5eeeeba
AbeTavarez <abeincwireless@gmail.com>
2021-06-05 23:49:30 -0400

    unlucky nums

--- Python/Short-exercises/unlucky_nums.py
+++ Python/Short-exercises/unlucky_nums.py
@@ -6,6 +6,7 @@ lucky_num = int(input('What is you favorite number? '))
 
 def unlucky_nums(int: lucky_num) -> dict:
     nums_dict = {}
+    
     for n in range(1,21):
         if n == lucky_num:
             nums_dict[n] = 'Lucky!!!'

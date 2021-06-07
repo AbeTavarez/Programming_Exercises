9fd7f8c7ff7b3a406269d9f2cf18b46c3fc52d3b
Abraham E. Tavarez <abetavarez@icloud.com>
2021-06-06 22:26:38 -0400

    encoding

--- Python/Projects/Caesar_Cipher/main.py
+++ Python/Projects/Caesar_Cipher/main.py
@@ -3,7 +3,20 @@ import string
 
 def caesar_cipher():
     alphabet = string.ascii_lowercase
-    print(alphabet)
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    plain_msg = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    def encrypt(msg, shiftby):
+        new_msg = ''
+        indexes = []
+        for char in msg:
+            indexes.append(alphabet.index(char))
+        for idx in indexes:
+            new_msg += alphabet[idx+shift]
+        print(new_msg)
+    encrypt(plain_msg, shift)
 
 
 caesar_cipher()
